af5975f6bb58d826552f375a2f380635b759dda0
kashi <kashireddynaani9967@gmail.com>
2025-10-15 22:47:40 +0530

    Hello world script

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Hi, Im learning devops with aws"
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: hi $PERSON2"
+echo "$PERSON2: hi $PERSON1 how r u "
+echo "$PERSON1: im fine $PERSON2"
