e533a15ec004384a1f39eaba4588173d307826f0
kashi <kashireddynaani9967@gmail.com>
2025-10-15 22:41:34 +0530

    Hello world script

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -2,8 +2,8 @@
 
 echo "Hi, Im learning devops with aws"
 
-PERSON1=Ramesh
-PERSON2=Suresh
+PERSON1=Sachin
+PERSON2=Rohot
 echo "$PERSON1: hi $PERSON2"
 echo "$PERSON2: hi $PERSON1 how r u "
 echo "$PERSON1: im fine $PERSON2"
