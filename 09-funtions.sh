7b94d35b53c887efc66305100f6d03d5fb9adb33
kashi <kashireddynaani9967@gmail.com>
2025-10-16 11:19:35 +0530

    Hello world script

---
+++ 09-funtions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USERID=$(id -u)
+# This fn should validate the previous command and tell whther it is success or not
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then    
+        echo "Installation.... FAILURE"
+        exit 1
+    else
+        echo "Installation......success"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then    
+    echo "not a root user, login with root user"
+    exit 1  
+else
+    echo "Root user, proceed with installations"
+fi
+
+yum install git -y
+
+VALIDATE $?
+
+yum install postfix -y
+
+VALIDATE $?
